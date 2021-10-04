c1f7beccbb97a57ae50bf5b7b71dc84f65853fa6
sweetbreeze-wang <80690323+sweetbreeze-wang@users.noreply.github.com>
2021-10-04 17:17:44 +0800

    Update main.go

--- httpserver/main.go
+++ httpserver/main.go
@@ -4,29 +4,19 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	exnet "ip"
 	"log"
 	"net/http"
-
 	_ "net/http/pprof"
-
-	"github.com/golang/glog"
+	"os"
 )
 
 func main() {
 	flag.Set("v", "4")
-	glog.V(2).Info("Starting http server...")
+	//glog.V(2).Info("Starting http server...")
 	http.HandleFunc("/", rootHandler)
-	c, python, java := true, false, "no!"
-	fmt.Println(c, python, java)
+	http.HandleFunc("/healthz", healthz)
 	err := http.ListenAndServe(":80", nil)
-	// mux := http.NewServeMux()
-	// mux.HandleFunc("/", rootHandler)
-	// mux.HandleFunc("/healthz", healthz)
-	// mux.HandleFunc("/debug/pprof/", pprof.Index)
-	// mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	// mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-	// mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
-	// err := http.ListenAndServe(":80", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -34,19 +24,37 @@ func main() {
 }
 
 func healthz(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "ok\n")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "OK\n")
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("entering root handler")
 	user := r.URL.Query().Get("user")
 	if user != "" {
 		io.WriteString(w, fmt.Sprintf("hello [%s]\n", user))
 	} else {
 		io.WriteString(w, "hello [stranger]\n")
 	}
-	io.WriteString(w, "===================Details of the http request header:============\n")
+	//写入服务端的状态码随便写了一个状态码
+	//w.WriteHeader(http.StatusContinue)
+	//fmt.Fprintln(w, "状态码正常，能够访问")
+	//返回客户端的头
+	io.WriteString(w, "======Detail of the request header :=====\n")
 	for k, v := range r.Header {
 		io.WriteString(w, fmt.Sprintf("%s=%s\n", k, v))
 	}
+	//返回version 环境变量
+	io.WriteString(w, "======Detail of Os VERSION:\n")
+	io.WriteString(w, os.Getenv("VERSION"))
+	io.WriteString(w, "\n")
+
+	//返回客户端IP
+	ip := exnet.ClientPublicIP(r)
+	if ip == "" {
+		ip = exnet.ClientIP(r)
+	}
+	io.WriteString(w, "======Detail of Client IP:")
+	io.WriteString(w, ip)
+	io.WriteString(w, "\n")
 }
+
